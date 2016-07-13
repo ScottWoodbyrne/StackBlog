045438e42273e35db564255f5aa336f917c0d8c0
ScottWoodbyrne <scott.woodbyrne@gmail.com>
2016-07-13 15:42:39 +0100

    update

--- blog/urls.py
+++ blog/urls.py
@@ -2,11 +2,11 @@ from django.conf.urls import url
 import views
 
 urlpatterns = [
-    url(r'^blog/$', views.post_list),
-    url(r'^blog/top$', views.post_list_top),
-    url(r'^blog/shortest', views.post_list_shortest),
-    url(r'^blog/(?P<id>\d*)/$', views.post_detail ),
+    url(r'^blog/$', views.post_list, name='home'),
+    url(r'^blog/top$', views.post_list_top,name='top'),
+    url(r'^blog/shortest', views.post_list_shortest, name='shortest'),
+    url(r'^blog/(?P<id>\d+)/$', views.post_detail ,name='view_post'),
     url(r'^blog/post/new/$', views.new_post, name='new_post'),
-    url(r'^blog/(?P<id>\d*)/edit$', views.edit_post),
+    url(r'^blog/(?P<id>\d+)/edit$', views.edit_post,name='edit_post'),
 ]
 
